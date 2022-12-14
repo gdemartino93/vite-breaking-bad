e1477fac52ad3880c7a6b52df77a06350da63fc0
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-12-14 13:43:18 +0100

    installed npm

--- src/main.js
+++ src/main.js
@@ -2,4 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+import "bootstrap/dist/css/bootstrap.css"
+import axios, {isCancel, AxiosError} from 'axios';
+
 createApp(App).mount('#app')
