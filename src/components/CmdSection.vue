99aae7394d8f24381704a90a48a55031be71af7e
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-12-15 17:43:46 +0100

    change option

--- src/components/CmdSection.vue
+++ src/components/CmdSection.vue
@@ -13,13 +13,12 @@ export default{
         
 <div class="col-lg-2 col-5 my-4 offset-1">
     <select class="form-select " aria-label=".form-select-sm example" v-model="store.selected" @change="$emit('ricerca')">
-        <option selected value="Starter">Select Category</option>
+        <option selected value="Starter">All</option>
             <option value="Alive">Alive</option>
             <option value="Dead">Dead</option>
             <option value="Unknown">Unknown</option>
            
     </select>
-    <p>{{store.selected}}</p>
 </div>
 
 </template>
